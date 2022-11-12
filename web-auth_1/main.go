7ac81c298cc66e39f1cc931dbd88fd0e8e2ebe82
Oleksandr Tsyhanov <tsyhanov@gmail.com>
2022-11-12 12:17:02 +0200

    split register and login handlers. Add password and hash comparing

--- web-auth_1/main.go
+++ web-auth_1/main.go
@@ -12,37 +12,47 @@ import (
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("HTTP Server started")
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	errorMsg := r.FormValue("errormsg")
+	errMsg := r.FormValue("errormsg")
 	fmt.Fprintf(w, `
 	<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <center> <h1> Login Form </h1> </center>  
-    <form action="/register" method="POST">  
-        <div class="container">   
-            <label>Email : </label>   
-            <input type="email" placeholder="Enter Email" name="email" required>  
-            <label>Password : </label>   
-            <input type="password" placeholder="Enter Password" name="password" required>  
-            <button type="submit">Login</button>   
-			<center> <h3> %s </h3> </center>  
-        </div>   
-    </form>     
-</body>
-</html>
-	`, errorMsg)
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+	</head>
+	<body>
+		<center> <h1> The error: %s </h1> </center>  
+		<center> <h1> Register Form </h1> </center>  
+		<form action="/register" method="POST">  
+			<div class="container">   
+				<label>Email : </label>   
+				<input type="email" placeholder="Enter Email" name="email" required>  
+				<label>Password : </label>   
+				<input type="password" placeholder="Enter Password" name="password" required>  
+				<button type="submit">Login</button>   
+			</div>   
+		</form> 
+		<h1>LOG IN</h1>
+		<form action="/login" method="POST">
+			<div class="container">   
+				<label>Email : </label>   
+				<input type="email" placeholder="Enter Email" name="email" required>  
+				<label>Password : </label>   
+				<input type="password" placeholder="Enter Password" name="password" required>  
+				<button type="submit">Login</button>   
+		</form>>    
+	</body>
+	</html>
+	`, errMsg)
 }
 
 var db = map[string][]byte{} //to store email and pass
@@ -51,20 +61,20 @@ func register(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
 		errorMsg := url.QueryEscape("method not a POST")
-		http.Redirect(w, r, "/&errormsg="+errorMsg, http.StatusSeeOther)
+		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
 		return
 	}
 
 	e := r.FormValue("email")
 	if e == "" {
 		errorMsg := url.QueryEscape("email is empty")
-		http.Redirect(w, r, "/&errormsg="+errorMsg, http.StatusSeeOther)
+		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
 		return
 	}
 	p := r.FormValue("password")
 	if p == "" {
 		errorMsg := url.QueryEscape("password is empty")
-		http.Redirect(w, r, "/&errormsg="+errorMsg, http.StatusSeeOther)
+		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
 		return
 	}
 
@@ -80,4 +90,44 @@ func register(w http.ResponseWriter, r *http.Request) {
 
 	db[e] = bsp
 	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+//login method
+func login(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		errorMsg := url.QueryEscape("method not a POST")
+		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	e := r.FormValue("email")
+	if e == "" {
+		errorMsg := url.QueryEscape("email is empty")
+		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+	p := r.FormValue("password")
+	if p == "" {
+		errorMsg := url.QueryEscape("password is empty")
+		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	//check if email is in use
+	if _, ok := db[e]; !ok {
+		errorMsg := url.QueryEscape("email is new")
+		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword(db[e], []byte(p))
+	if err != nil {
+		errorMsg := url.QueryEscape("Your email or password is incorrect")
+		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	errorMsg := url.QueryEscape("You logged in " + e)
+	http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
 }
